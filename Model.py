380ecde55c91b2be668607adc1ca0bee45aa2eee
antony <ampaxmusic6@gmail.com>
2023-01-18 13:02:25 -0800

    change

--- Model.py
+++ Model.py
@@ -4,9 +4,16 @@ conn = sqlite3.connect('password.db')
 
 c = conn.cursor()
 
-c.execute("""CREATE TABLE passwords (
-            first text,
-            last text,
-            password text
-            )""")
+# c.execute("""CREATE TABLE passwords (
+#             first text,
+#             last text,
+#             password text
+#             )""")
 
+# c.execute("INSERT INTO passwords VALUES ('Site', 'Username', 'Password', 'link')
+
+c.execute("SELECT ")
+
+conn.commit()
+
+conn.clone()
