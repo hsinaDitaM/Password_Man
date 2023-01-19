7a15cde87da1b6e08321c19c8ee8272bdacdbaf5
antony <ampaxmusic6@gmail.com>
2023-01-19 10:09:51 -0800

    change

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask
 from flask import render_template
+from CreateAccount import registration
 
 app = Flask(__name__)
 app.secret_key="__privatekey__"
@@ -12,16 +13,9 @@ def defaultHome():
 def __init__(self):
     conn = sqlite3.connect('user.db')
     c = conn.cursor()
-    c.execute("""CREATE TABLE users (
-                username text,
-                password text
-                )""")
-
-    username = """username"""
-    password = """password"""
     c.execute("CREATE TABLE user1(name text, password text")
 
     conn.commit()
     conn.close()
 
-    @app.route('/CreateAccount',methods=['POST','GET'])
+@app.route('/registration',methods=['POST','GET'])
