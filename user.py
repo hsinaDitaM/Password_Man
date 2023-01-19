14b90db5950cfa4a698b24231e154e30842775fc
antony <ampaxmusic6@gmail.com>
2023-01-19 11:04:18 -0800

    add error page

--- user.py
+++ user.py
@@ -2,6 +2,7 @@ import sqlite3
 from flask import Flask
 from flask import render_template
 from CreateAccount import registration
+import requests
 
 app = Flask(__name__)
 app.secret_key="__privatekey__"
@@ -19,3 +20,21 @@ def __init__(self):
     conn.close()
 
 @app.route('/registration',methods=['POST','GET'])
+def registrationform():
+    registrations = registration()
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    if requests.method=='POST':
+        if(requests.form["name"]!="" and requests.form["password"]!=""):
+            name=requests.form["name"]
+            password=requests.form["password"]
+            statement = f"SELECT * from user WHERE name='{name}' AND password='{password}';"
+            c.execute(statement)
+            data=c.fetchone()
+            if data:
+
+
+
+    elif requests.method=='GET':
+        return render_template('CreateAccount.html',form=registrations)
+    '
